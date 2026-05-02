4c3a9aa3ff2b794f3c131a36b0598930b69c6204
Udeshna <114094749+Udeshna-labs@users.noreply.github.com>
2026-05-02 20:12:59 +0530

    ping.py

--- NetProbe/ping.py
+++ NetProbe/ping.py
@@ -1,1 +1,15 @@
 
+import platform
+import subprocess
+
+def ping_host(host: str) -> bool:
+    param = "-n" if platform.system().lower() == "windows" else "-c"
+    command = ["ping", param, "1", host]
+
+    result = subprocess.run(
+        command,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+    return result.returncode == 0
